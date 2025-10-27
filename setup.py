3ce071875a7151dbcb4561a9986d57646aac3d9e
SerJo2 <termenator1ser@gmail.com>
2025-10-28 00:12:50 +1000

    version push

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='hentailib',
-    version='0.5.0',
+    version='0.5.1',
     packages=[''],
     url='https://github.com/SerJo2/hentailib',
     license='MIT',
